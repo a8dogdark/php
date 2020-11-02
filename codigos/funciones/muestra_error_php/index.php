ec4c80a45d74323cde018af0c0feb40ce3a57347
dogdark <42787966+a8dogdark@users.noreply.github.com>
2020-11-02 12:34:00 -0300

    Update index.php

--- codigos/funciones/muestra_error_php/index.php
+++ codigos/funciones/muestra_error_php/index.php
@@ -1,3 +1,6 @@
 <?php
 require_once('clases/config.php');
+//llamamos a la funcion que muestra en pantalla los errores generados por mal implementacion de codigo php
+error_php();
+//resto del codigo hacia abajo
 ?>
