7969526032e1a939a63c825a2795ec9ded7e9081
dogdark <42787966+a8dogdark@users.noreply.github.com>
2020-11-02 12:36:18 -0300

    Update config.php

--- codigos/funciones/muestra_error_php/clases/config.php
+++ codigos/funciones/muestra_error_php/clases/config.php
@@ -1,1 +1,8 @@
-
+<?php
+//funcion que muestra los errores de php cuando el servidor no lo hace
+function error_php()
+{
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+}
